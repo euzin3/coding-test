dc1e09ae1341ade9351af04669c98c0dc5bf4f9b
euzin3 <reason_jin7@naver.com>
2026-01-12 08:06:01 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -2,14 +2,8 @@
 using namespace std;
 
 int main(){
-	unsigned long long A=0;
-	unsigned long long B=0;
-	unsigned long long C=0;
-	unsigned long long sum = 0;
-
-	cin >> A >> B >> C;
-	sum = A + B + C; 
-	cout << sum;
-	
+	long long A, B, C;
+	cin >> A >> B >> C; 
+	cout << A + B + C;
 	return 0;
 } 
