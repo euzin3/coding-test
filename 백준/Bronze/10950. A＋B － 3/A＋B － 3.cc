315671fe54204b966075b2c77554f10d461e60a5
euzin3 <reason_jin7@naver.com>
2026-01-15 17:14:58 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int A,B,T;
+    cin >> T;
+
+    for(int i=0; i<T; i++){
+        cin >> A >> B;
+        cout << A+B << "\n";
+    }
+    return 0;
+}
